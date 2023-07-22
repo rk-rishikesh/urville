b371a3edfca23140a4e3d7b62efc3a9e76c516a5
rk-rishikesh <rvk.rishikeshkale@gmail.com>
2023-07-22 13:32:17 +0530

    Categorized DApps

---
+++ src/data.js
@@ -0,0 +1,51 @@
+export const nft = [
+    "rarible.app",
+    "mintify.app",
+    "surfaceboard.app",
+    "opensea.app",
+    "heymint.app",
+    "sunflower-land.app",
+    "komet.app",
+    "socialswap.app",
+    "socialswap-transfer.app",
+    "mocaverse.app",
+    "pxcard.app",
+    "jump.app",
+    "floornfts.app",
+    "socialswap-mart.app",
+    "oohlala.app"
+]
+
+export const defi = [
+    "1inch.app",
+    "coingecko.app",
+    "reap.app",
+    "aave.app",
+    "curve.app",
+    "uniswap.app",
+    "sushi.app",
+    "balancer.app",
+    "yearn.app",
+    "crypto.app"
+]
+
+export const social = [
+    "sending.app",
+    "dmail.app",
+    "decentraland.app",
+    "main.app",
+    "lens.app",
+    "push-app.app",
+    "soclly.app",
+    "huddln.app",
+    "unjumble.app",
+    "phaver.app"
+]
+
+export const game = [
+    "milliononmars.app",
+    "sparkball.app",
+    "wildcard.app",
+    "wam.app",
+    "frontyardbaseball.app"
+]
